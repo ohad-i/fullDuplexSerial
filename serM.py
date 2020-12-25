50f1033786c0ccae5ddc356b889eb027f1f87662
ohadi <ohadon@gmail.com>
2020-12-25 18:21:30 +0200

    update

--- serM.py
+++ serM.py
@@ -8,6 +8,7 @@ import time
 parser = argparse.ArgumentParser(description='Config RFD modem')
 parser.add_argument('-p', '--port', default='/dev/ttyUSB0', help='RFD modem port, default: /dev/ttyUSB0')
 parser.add_argument('-b', '--baudRate', type=int, default=57600, help='Modem baud rate, default: 57600 ')
+parser.add_argument('-w', '--writer', action='store_true', help='is writer serial (or reciever) ')
 
 args = parser.parse_args()
 
@@ -25,14 +26,25 @@ def sendData(ser, data, chunckSize=200):
 
 
 
+def recieveData(ser):
+
+    ret = ''
+    dataWaiting = ser.in_waiting
+    if dataWaiting > 0:
+        ret = ser.read(dataWaiting)
+    return ret
+
+
+    
 
 
 if __name__=='__main__':
 
     baud = args.baudRate
     port = args.port
-    ser = Serial(port, baud, write_timeout=0, inter_byte_timeout=None, exclusive=False)#, timeout=1)
-    ser1 = Serial(port, baud, write_timeout=0, inter_byte_timeout=None, exclusive=False)#, timeout=1)
+    isWriter = args.writer
+
+    ser = Serial(port, baud, write_timeout=0, inter_byte_timeout=None, exclusive=False)
     
 
     dataSum = int((0.75*baud)//8//2)
@@ -43,17 +55,36 @@ if __name__=='__main__':
 
     dataSum = 0.0
     tic = time.time()
-    while True:
-        time.sleep(0.0001)
-        #dataSum += sendData(ser, oneSecData)
-        dataSum += sendData(ser, b'00112233445566'*1)
-        if time.time() - tic >= 5:
-            bps = (dataSum*8)/(time.time() - tic)
-            print('sent bps: %0.2f[bps] effective baud rate: %0.2f'%(bps, bps/baud) ) 
-            tic = time.time()
-            dataSum = 0.0
-
     
+
+    if isWriter:
+        print('serial writer initialized...')
+        while True:
+            time.sleep(0.0001)
+            #dataSum += sendData(ser, oneSecData)
+
+            dataSum += sendData(ser, b'00112233445566'*20)
+            if time.time() - tic >= 5:
+                bps = (dataSum*8)/(time.time() - tic)
+                print('%s sent bps: %0.2f[bps] effective baud rate: %0.2f'%(port, bps, bps/baud) ) 
+                tic = time.time()
+                dataSum = 0.0
+    else:
+        print('serial reader initialized...')
+        while True:
+            time.sleep(0.0001)
+
+            data = recieveData(ser)
+            dataSum += len(data)
+            if time.time() - tic >= 5:
+                bps = (dataSum*8)/(time.time() - tic)
+                print('%s recieved bps: %0.2f[bps] effective baud rate: %0.2f'%(port, bps, bps/baud) ) 
+                tic = time.time()
+                dataSum = 0.0
+
+
+
+        
 
 
 
